cad35e326ab09f01675496c464bb08ed3d012236
TTanishq27 <tanishq2444.be22@chitkara.edu.in>
2025-07-29 21:09:52 +0530

    Final

--- api/index.js
+++ api/index.js
@@ -1,10 +1,7 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
 
 const app = express();
-app.use(bodyParser.json());
-app.use(cors);
+app.use(express.json());
 
 const FULL_NAME = "john_doe";
 const DOB = "17091999";
